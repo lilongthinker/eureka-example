a3689b01c46b2661758f1a5e459f5612b08a87c1
lilongthinker <lilongthinker@163.com>
2019-12-28 14:04:02 +0800

    spirnt start inti project

---
+++ eureka-web/src/main/java/com/alibaba/eurekaweb/EurekaWebApplication.java
@@ -0,0 +1,13 @@
+package com.alibaba.eurekaweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaWebApplication.class, args);
+	}
+
+}
